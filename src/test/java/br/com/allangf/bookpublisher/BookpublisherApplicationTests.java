cd508c06b10ff219e534c8ee55d8416a534083ef
AllanGF <allan201gf@gmail.com>
2024-10-16 16:49:39 -0300

    First commit

---
+++ src/test/java/br/com/allangf/bookpublisher/BookpublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.allangf.bookpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookpublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
