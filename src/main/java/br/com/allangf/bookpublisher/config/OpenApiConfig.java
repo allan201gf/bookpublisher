ffc30933c47dde9a8f51b256ac27808d25af1ce5
AllanGF <allan201gf@gmail.com>
2024-10-16 23:10:04 -0300

    Implemented basic auth

--- src/main/java/br/com/allangf/bookpublisher/config/OpenApiConfig.java
+++ src/main/java/br/com/allangf/bookpublisher/config/OpenApiConfig.java
@@ -1,16 +1,29 @@
 package br.com.allangf.bookpublisher.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(
-        title = "Book Publisher",
-        version = "1.0.0",
-        description = "Inventory control of a publisher",
-        contact = @Contact(name = "Allan Garcia Ferreira",
-                email = "allan201gf@gmail.com")))
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Book Publisher",
+                version = "1.0.0",
+                description = "Inventory control of a publisher",
+                contact = @Contact(name = "Allan Garcia Ferreira",
+                        email = "allan201gf@gmail.com")),
+        security = {
+                @SecurityRequirement(name = "basicAuth")
+        }
+)
+@SecurityScheme(
+        name = "basicAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "basic"
+)
 public class OpenApiConfig {
 }
---
+++ src/main/java/br/com/allangf/bookpublisher/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package br.com.allangf.bookpublisher.config;
+
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${auth.user}")
+    @Setter
+    private String user;
+
+    @Value("${auth.pass}")
+    @Setter
+    private String pass;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder
+                .inMemoryAuthentication()
+                .withUser(user)
+                .password(passwordEncoder().encode(pass))
+                .roles("ADMIN");
+
+        return authenticationManagerBuilder.build();
+    }
+}
